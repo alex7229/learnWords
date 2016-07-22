eafb80f5ef77e601cb33322863a0b69ae8d89ede
tup1tsa <tup1tsa@rambler.ru>
2016-07-22 22:10:44 +0300

    Add power to words (somehow duplications appeared)

--- main.js
+++ main.js
@@ -1,10 +1,46 @@
-let words = [];
+/*
+ parsing from txt.rawData into JSON
+ let rawData = data;
+ let regExpGroups = /----- \d{1,2} -----/g;
+ let wordGroups = rawData.split(regExpGroups);
+ let bigList = [];
+ let wordNumber = 0;
+ for (let i=1; i<=21; i++) {
+ let group = wordGroups[i];
+ let regExpWords = /(\w+)\r\n(?:\s{4}([ \w,*]+)\r\n)?/gi;
+ let regExpResult;
+ while ((regExpResult = regExpWords.exec(group)) !== null) {
+ wordNumber++;
+ let word = {
+ word: regExpResult[1],
+ number: wordNumber,
+ group: i,
+ synonyms: regExpResult[2]
+ };
+ bigList.push(word)
+ }
+ }
+ console.log(JSON.stringify(bigList))
+ */
 $.ajax({
-    url: 'http://tup1tsa.bounceme.net/learnWords/words.txt'
+    url: 'http://tup1tsa.bounceme.net/learnWords/wordsLists/sorted_34k.txt'
 })
 .done((data) => {
-    words = JSON.parse(data);
+   let words = data;
+    function compare (a,b) {
+        if (a.power>b.power) {
+            return -1
+        }
+        if (a.power<b.power) {
+            return 1
+        }
+        return 0
+    }
+    words.sort(compare);
     console.log(words)
+
+
+
 });
 
-//todo delete words, which has no definition on yandex.dictionary
+
