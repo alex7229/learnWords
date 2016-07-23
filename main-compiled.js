b60fe68f9272f6b4a5cbb7f6da574aada3d37d4c
tup1tsa <tup1tsa@rambler.ru>
2016-07-23 13:50:57 +0300

    Add word list data (sorted by power main words).

--- main-compiled.js
+++ main-compiled.js
@@ -1,15 +1,13 @@
-'use strict';
-
 /*
  parsing from txt.rawData into JSON
- let rawData = data;
+ let rawData = data.toLowerCase().replace(/[!)(]/g, '');
  let regExpGroups = /----- \d{1,2} -----/g;
  let wordGroups = rawData.split(regExpGroups);
  let bigList = [];
  let wordNumber = 0;
  for (let i=1; i<=21; i++) {
  let group = wordGroups[i];
- let regExpWords = /(\w+)\r\n(?:\s{4}([ \w,*]+)\r\n)?/gi;
+ let regExpWords = /([\w'-]+)\r\n(?:\s{4}([ \w,*'-]+)\r\n)?/g;
  let regExpResult;
  while ((regExpResult = regExpWords.exec(group)) !== null) {
  wordNumber++;
@@ -17,28 +15,71 @@
  word: regExpResult[1],
  number: wordNumber,
  group: i,
- synonyms: regExpResult[2]
+ differentSpellings: regExpResult[2]
  };
- bigList.push(word)
+ if (!word.word.match(/-/)) {
+ bigList.push(word);
  }
  }
- console.log(JSON.stringify(bigList))
+ }
+ console.log(JSON.stringify(bigList));
  */
+
+/*get sorted Data
 $.ajax({
     url: 'http://tup1tsa.bounceme.net/learnWords/wordsLists/sorted_34k.txt'
-}).done(function (data) {
-    var words = JSON.parse(data)
-    function compare(a, b) {
-        if (a.power > b.power) {
-            return -1;
-        }
-        if (a.power < b.power) {
-            return 1;
-        }
-        return 0;
-    }
-    words.sort(compare);
-    console.log(words);
+})
+.done((data) => {
+    let mainWords = JSON.parse(data);
+    $.ajax({
+            url: 'http://tup1tsa.bounceme.net/learnWords/wordsLists/rawLists/300k.txt'
+        })
+        .done((data) => {
+            let superBigList = [];
+
+            let powerWordsRaw = data;
+            let regExp = /(\w+)\s+(\d+)/g;
+            let regExpResult;
+            while ((regExpResult = (regExp.exec(powerWordsRaw)))!==null) {
+                superBigList.push({
+                    word: regExpResult[1],
+                    power: parseInt(regExpResult[2])
+                })
+            }
+
+
+            let power = 3000;
+            for (let i=0; i<mainWords.length; i++) {
+                let mainWord = mainWords[i];
+                for (let j=0; j<superBigList.length; j++) {
+                    let powerWord = superBigList[j];
+                    if (mainWord.word === powerWord.word) {
+                        mainWord.power = powerWord.power;
+                        break
+                    }
+                }
+                if (!mainWord.power) {
+                    mainWord.power = power;
+                    power--
+                }
+            }
+
+
+            function compare (a,b) {
+                if (a.power>b.power) return -1;
+                if (a.power<b.power) return 1;
+                return 0
+            }
+            mainWords.sort(compare);
+            console.log(JSON.stringify(mainWords))
+
+
+        });
+
 });
+*/
+
+//todo - get to work google.api for dictionary (via suspicious site) and yandex.api for translations
+"use strict";
 
 //# sourceMappingURL=main-compiled.js.map
